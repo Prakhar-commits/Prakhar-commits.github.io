04e103d273e5b121cbfec5a02127dc762baf5eff
Prakhar <prakharb56@gmail.com>
2022-09-30 19:38:36 +0530

    changes for form validation

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 const dynamicContent = document.getElementById("dynamic-text");
-const phrases = ["Student...", "Psychology-enthusiast...", "Video-editor...", "<and I just love my Coffee...>"];
-let phraseIndex =0;  
+const phrases = [
+  "Student...",
+  "Psychology-enthusiast...",
+  "Video-editor...",
+  "<and I just love my Coffee...>",
+];
+let phraseIndex = 0;
 let letterIndex = 0;
 const typingspeed = 100;
 const erasingSpeed = 125;
 
-
-
-function printletter(phrase){
-    if(letterIndex == phrase.length){
-        clearletter();
-    }
-    else if(letterIndex < phrase.length){
-        dynamicContent.textContent += phrase.charAt(letterIndex);
-        letterIndex += 1;
-        setTimeout(function(){
-            printletter(phrase);
-        },typingspeed)
-    }
-
+function printletter(phrase) {
+  if (letterIndex == phrase.length) {
+    clearletter();
+  } else if (letterIndex < phrase.length) {
+    dynamicContent.textContent += phrase.charAt(letterIndex);
+    letterIndex += 1;
+    setTimeout(function () {
+      printletter(phrase);
+    }, typingspeed);
+  }
 }
 
 function clearletter() {
-    if(letterIndex == -1){
-        phraseIndex = (phraseIndex+1) % phrases.length;
-        letterIndex = 0;
-        printletter(phrases[phraseIndex])
+  if (letterIndex == -1) {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    letterIndex = 0;
+    printletter(phrases[phraseIndex]);
+  } else if (letterIndex > -1) {
+    let updatedphrase = "";
+    for (let index = 0; index < letterIndex; index++) {
+      updatedphrase += phrases[phraseIndex].charAt(index);
     }
-        else if(letterIndex > -1){
-            let updatedphrase = "";
-            for(let index = 0 ;index < letterIndex; index++) {
-                updatedphrase += phrases[phraseIndex].charAt(index);
-
-            }
-            console.log(updatedphrase);
-            dynamicContent.textContent = updatedphrase;
-            letterIndex -= 1;
-            setTimeout(clearletter, erasingSpeed);
-        }
+    console.log(updatedphrase);
+    dynamicContent.textContent = updatedphrase;
+    letterIndex -= 1;
+    setTimeout(clearletter, erasingSpeed);
+  }
 }
 
 printletter(phrases[phraseIndex]);
